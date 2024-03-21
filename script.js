3a12315efea2d10dee0388765e4f7aee30289640
Anna Mariakhina <a.mariakhina@gmail.com>
2024-03-21 16:29:46 -0600

    finidshed all buttons

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
+const counterEl = document.querySelector('.counter');
 const increaseButtonEl = document.querySelector('.counter__button--increase');
 const decreaseButtonEl = document.querySelector('.counter__button--decrease');
 const resetButtonEl = document.querySelector('.counter__reset-button');
-
 const counterValueEl = document.querySelector('.counter__value');
+const counterTitleEl = document.querySelector('.counter__title');
 
-resetButtonEl.addEventListener('click', function(){
+resetButtonEl.addEventListener('click', () => {
   counterValueEl.textContent = 0;
-})
+  counterEl.classList.remove('counter--limit');
+  counterTitleEl.textContent = 'Fancy counter';
+  increaseButtonEl.disabled = false;
+  decreaseButtonEl.disabled = false;
+  resetButtonEl.blur();
+});
 
-function incrementCounter() {
+const incrementCounter = () =>{
   const currentValue = counterValueEl.textContent;
 
   const currentValueAsNumber =+currentValue;
   let newValue = currentValueAsNumber + 1;
       
-  if (newValue >= 5 ){
+  if (newValue > 5 ){
     newValue = 5;
-    
+    counterEl.classList.add('counter--limit');
+    counterTitleEl.innerHTML = 'Limit! Buy <b>Pro</b> for > 5';
+   increaseButtonEl.disabled = true;
+  decreaseButtonEl.disabled = true;
   } 
   counterValueEl.textContent = newValue;
+  resetButtonEl.blur();
+
 }
 
 
 
-decreaseButtonEl.addEventListener('click', function() {
+decreaseButtonEl.addEventListener('click', () => {
   const currentValue = counterValueEl.textContent;
   const currentValueAsNumber =+currentValue;
   let newValue = currentValueAsNumber - 1;
@@ -31,6 +42,8 @@ decreaseButtonEl.addEventListener('click', function() {
     newValue = 0;
   }
   counterValueEl.textContent = newValue;
+    resetButtonEl.blur();
+
 })
 
 increaseButtonEl.addEventListener('click', incrementCounter);
